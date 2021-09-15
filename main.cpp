e1b097da6f075ce39f16135f372d49ee07bc0189
Nata0110 <87442131+Nata0110@users.noreply.github.com>
2021-09-15 20:40:33 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define return puts("Hello world_1");return
+//#include <cstdio>
+int word = printf("Hello world_2\n");
+
+int printHelloWorld() { printf("Hello World_3\n"); return 0; }
+static int testVar = printHelloWorld();
+int main ()
+{
+
+}
